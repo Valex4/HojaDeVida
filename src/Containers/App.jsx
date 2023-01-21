e79da62c3e5af1976cb3dec878bdcabdf76a7297
Valex4 <yam778123@gmail.com>
2023-01-21 14:54:23 -0600

    ajustando css

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Landing from '../pages/Landing';
-import NotFound from '../pages/NotFound';
+
 
 function App() {
     return ( 
